e3c36c060169c500d3ea74cb147b71e12fe8ec9b
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-04-14 18:18:31 -0300

    Restyle - criando o tema no App

--- App.tsx
+++ App.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import { SafeAreaView, View } from 'react-native';
 import { Text } from './src/components/Text/Text';
 import { Button } from './src/components/BUtton/Button';
+import { ThemeProvider } from '@shopify/restyle';
+import { theme } from './src/theme/theme';
 
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <View style={{ paddingHorizontal: 24 }}>
-        <Text preset='headingLarge' italic >Coffstack</Text>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{ paddingHorizontal: 24 }}>
+          <Text preset='headingLarge'
+            italic >
+            Coffstack
+          </Text>
 
-        <Button
-          title='Clique aqui'
-        />
+          <Button
+            title='Entrar'
+          />
 
-      </View>
-    </SafeAreaView>
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   )
 }
 
--- src/components/BUtton/Button.tsx
+++ src/components/BUtton/Button.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
 import { Text } from '../Text/Text'
+import { useTheme } from '@shopify/restyle'
+import { Theme } from '../../theme/theme'
 
 interface ButtonProps {
     title: string
 }
 
 export function Button({ title }: ButtonProps) {
+    const { colors } = useTheme<Theme>()
+
     return (
         <TouchableOpacity
             style={{
                 paddingHorizontal: 20,
                 paddingVertical: 14,
-                backgroundColor: '#074c4e',
+                backgroundColor: colors.success,
                 alignItems: 'center',
                 borderRadius: 16,
 
