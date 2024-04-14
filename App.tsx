2d31ec3a3047474867bd1093c5e528de53164b83
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-04-14 18:30:27 -0300

    Box component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaView, View } from 'react-native';
 import { Text } from './src/components/Text/Text';
-import { Button } from './src/components/BUtton/Button';
+import { Button } from './src/components/Button/Button';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
 
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -35,7 +35,18 @@ export const theme = createTheme({
         successLight: palette.greenSuccessLight,
 
     },
-    spacing: {},
+    spacing: {
+        s4: 4,
+        s8: 8,
+        s12: 12,
+        s16: 16,
+        s20: 20,
+        s24: 24,
+        s32: 32,
+        s40: 40,
+        s48: 48,
+        s56: 56,
+    },
     borderRadii: {}
 })
 
