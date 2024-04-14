777671af523e61277425341be013b8d9d41717e9
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-04-14 17:53:53 -0300

    Crinado um button sem lib Restyle

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import { Text } from './src/components/Text/Text';
+import { Button } from './src/components/BUtton/Button';
 
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset='headingMedium' italic >Fonte Normal</Text>
-      <Text preset='headingMedium' italic bold style={{ color: 'red' }}>Fonte Satoshi-Light</Text>
-      <Text preset='headingMedium' semiBold style={{ color: 'red' }}>Fonte Satoshi-Light</Text>
+      <View style={{ paddingHorizontal: 24 }}>
+        <Text preset='headingLarge' italic >Coffstack</Text>
 
+        <Button
+          title='Clique aqui'
+        />
 
+      </View>
     </SafeAreaView>
   )
 }
---
+++ src/components/BUtton/Button.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Text } from '../Text/Text'
+
+interface ButtonProps {
+    title: string
+}
+
+export function Button({ title }: ButtonProps) {
+    return (
+        <TouchableOpacity
+            style={{
+                paddingHorizontal: 20,
+                paddingVertical: 14,
+                backgroundColor: '#074c4e',
+                alignItems: 'center',
+                borderRadius: 16,
+
+            }}
+        >
+            <Text
+                preset='headingMedium'
+                style={{
+                    color: '#fff',
+                }}
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    )
+}
