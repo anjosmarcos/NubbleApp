4e08eca44a67f74fecffdcb7ddcba1dc9bc55b5e
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-04-14 19:03:13 -0300

    TouchableOpacityBox - TypeScript e Interface

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,10 +1,20 @@
-import { backgroundColor, border, createBox, createRestyleComponent, layout, spacing } from '@shopify/restyle';
+import { backgroundColor, BackgroundColorProps, border, BorderProps, createBox, createRestyleComponent, layout, LayoutProps, spacing, SpacingProps, spacingShorthand, SpacingShorthandProps } from '@shopify/restyle';
 import { Theme } from '../../theme/theme';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps as RNTouchableOpacityProps } from 'react-native';
 
 export const Box = createBox<Theme>();
 
+export type TouchableOpacityBoxProps = SpacingProps<Theme>
+    & LayoutProps<Theme>
+    & BackgroundColorProps<Theme>
+    & RNTouchableOpacityProps
+    & BorderProps<Theme>;
 
-export const TouchOpacityBox = createRestyleComponent([
-    backgroundColor, spacing, layout, border
-], TouchableOpacity)
+export const TouchableOpacityBox = createRestyleComponent<
+    TouchableOpacityBoxProps,
+    Theme>([
+        backgroundColor,
+        spacing,
+        layout,
+        border
+    ], TouchableOpacity)
