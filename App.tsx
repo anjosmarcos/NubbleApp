43ef3001ceeae54d4098a5b66958ed4416571d2b
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-05-02 09:09:19 -0300

    useAppSafeArea

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from '@shopify/restyle';
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LoginScreen } from './src/screens/auth/LoginScreen/LoginSreen';
 import { theme } from './src/theme/theme';
 
 
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <LoginScreen />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <LoginScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   )
 }
 
--- src/screens/auth/LoginScreen/LoginSreen.tsx
+++ src/screens/auth/LoginScreen/LoginSreen.tsx
@@ -1,39 +1,36 @@
-import { SafeAreaView, View } from "react-native";
 import { Button } from "../../../components/BUtton/Button";
 import { Icon } from "../../../components/Icon/Icon";
+import { Screen } from "../../../components/Screen/Screen";
 import { Text } from "../../../components/Text/Text";
 import { TextInput } from "../../../components/TextInput/TextInput";
 
 export function LoginScreen() {
     return (
-        <SafeAreaView>
-            <View style={{ padding: 24 }}>
-                <Text marginBottom='s8' preset='headingLarge'>Olá!</Text>
-                <Text mb='s40' preset='paragraphLarge'>
-                    Digite seu e-mail e senha para entrar
-                </Text>
+        <Screen>
+            <Text marginBottom='s8' preset='headingLarge'>Olá!</Text>
+            <Text mb='s40' preset='paragraphLarge'>
+                Digite seu e-mail e senha para entrar
+            </Text>
 
-                <TextInput
-                    label='E-mail'
-                    placeholder='Digite seu e-mail'
-                    boxProps={{ mb: 's20' }}
-                />
+            <TextInput
+                label='E-mail'
+                placeholder='Digite seu e-mail'
+                boxProps={{ mb: 's20' }}
+            />
 
-                <TextInput
-                    label='Senha'
-                    placeholder='Digite sua senha'
-                    RightComponent={<Icon name='eyeOn' color='gray2' />}
-                    boxProps={{ mb: 's10' }}
-                />
+            <TextInput
+                label='Senha'
+                placeholder='Digite sua senha'
+                RightComponent={<Icon name='eyeOn' color='gray2' />}
+                boxProps={{ mb: 's10' }}
+            />
 
-                <Text mt='s10' color='primary' preset='paragraphSmall' >
-                    Esqueci minha senha
-                </Text>
+            <Text mt='s10' color='primary' preset='paragraphSmall' >
+                Esqueci minha senha
+            </Text>
 
-                <Button title='Entrar' marginTop='s48' />
-                <Button preset='outline' title='Criar uma conta' marginTop='s12' />
-
-            </View>
-        </SafeAreaView>
+            <Button title='Entrar' marginTop='s48' />
+            <Button preset='outline' title='Criar uma conta' marginTop='s12' />
+        </Screen>
     )
 }
