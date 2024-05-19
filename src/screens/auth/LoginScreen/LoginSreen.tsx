1abb1410431dbfe619ac750bcecb33ab3a579ca3
Marcos Alexandre <dev.marcosanjos@gmail.com>
2024-05-19 17:23:18 -0300

    Gerenciando Formulário sem Bibliotecas

--- src/screens/auth/LoginScreen/LoginSreen.tsx
+++ src/screens/auth/LoginScreen/LoginSreen.tsx
@@ -1,4 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
 import { Button } from "../../../components/BUtton/Button";
 import { Screen } from "../../../components/Screen/Screen";
 import { Text } from "../../../components/Text/Text";
@@ -11,12 +13,28 @@ type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginSreen'>
 
 export function LoginScreen({ navigation }: ScreenProps) {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailErrorMessages, setEmailErrorMessages] = useState('');
+
+    useEffect(() => {
+        const isValidateEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email);
+        setEmailErrorMessages(isValidateEmail ? '' : 'E-mail inválido')
+
+    }, [email])
+
     function navigateToSignUpScreen() {
         navigation.navigate('SingUpScreen');
     }
 
     function navigateToForgotPasswordScreen() {
         navigation.navigate('ForgotPasswordScreen')
+    }
+
+    function submitForm() {
+        // Chamar a API para fazer login
+
+        Alert.alert(`Email: ${email} ${`\n`}Senha: ${password}`)
     }
 
     return (
@@ -27,12 +45,17 @@ export function LoginScreen({ navigation }: ScreenProps) {
             </Text>
 
             <TextInput
+                errorMessage={emailErrorMessages}
+                value={email}
+                onChangeText={setEmail}
                 label='E-mail'
                 placeholder='Digite seu e-mail'
                 boxProps={{ mb: 's20' }}
             />
 
             <PasswordInput
+                value={password}
+                onChangeText={setPassword}
                 label='Senha'
                 placeholder='Digite sua senha'
                 // RightComponent={<Icon name='eyeOn' color='gray2' />}
@@ -43,7 +66,7 @@ export function LoginScreen({ navigation }: ScreenProps) {
                 Esqueci minha senha
             </Text>
 
-            <Button title='Entrar' marginTop='s48' />
+            <Button disabled={!!emailErrorMessages || password.length < 6} onPress={submitForm} title='Entrar' marginTop='s48' />
             <Button onPress={navigateToSignUpScreen} preset='outline' title='Criar uma conta' marginTop='s12' />
         </Screen>
     )
