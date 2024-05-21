71f4cab6edde02e40eb25ab06c004bd1bc8d9eb7
Marcos Alexandre <dev.marcosanjos@gmail.com>
2024-05-21 08:38:56 -0300

    Componente de Formulário - Don't Repeat Yourself (DRY)

--- src/screens/auth/SingUpScreen/SingUpScreen.tsx
+++ src/screens/auth/SingUpScreen/SingUpScreen.tsx
@@ -1,9 +1,9 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Controller, useForm } from "react-hook-form";
 import { Button } from "../../../components/BUtton/Button";
+import { FormTextInput } from "../../../components/FormTextInput/FormTextInput";
 import { Screen } from "../../../components/Screen/Screen";
 import { Text } from "../../../components/Text/Text";
-import { TextInput } from "../../../components/TextInput/TextInput";
 import { useResetNavigationSucess } from "../../../hooks/useResetNavigationSucess";
 import { RootStackParamList } from "../../../routes/routes";
 import { PasswordInput } from "./PasswordInput";
@@ -11,7 +11,7 @@ import { PasswordInput } from "./PasswordInput";
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SingUpScreen'>
 type SingUpFormType = {
     username: string;
-    fullName: string;
+    fullname: string;
     email: string;
     password: string;
 }
@@ -22,7 +22,7 @@ export function SingUpScreen({ navigation }: ScreenProps) {
     const { control, formState, handleSubmit } = useForm<SingUpFormType>({
         defaultValues: {
             username: '',
-            fullName: '',
+            fullname: '',
             email: '',
             password: ''
         },
@@ -45,70 +45,34 @@ export function SingUpScreen({ navigation }: ScreenProps) {
         <Screen canGoBack scrollable>
             <Text preset="headingLarge" mb="s32">Criar uma conta</Text>
 
-            <Controller
+            <FormTextInput
                 control={control}
+                autoCapitalize="none"
                 name="username"
-                rules={{
-                    required: 'Campo obrigatório',
-                    minLength: {
-                        value: 3,
-                        message: 'O username precisa ter no mínimo 3 caracteres'
-                    }
-                }}
-                render={({ field, fieldState }) => {
-                    return (
-                        <TextInput
-                            value={field.value}
-                            onChangeText={field.onChange}
-                            errorMessage={fieldState.error?.message}
-                            label="Seu username"
-                            placeholder="@"
-                            boxProps={{ mb: "s20" }}
-                        />
-                    )
-                }}
+                rules={{ required: 'Campo obrigatório' }}
+                label="Seu username"
+                placeholder="@"
+                boxProps={{ mb: "s20" }}
             />
 
-
-            <Controller
+            <FormTextInput
                 control={control}
-                name="fullName"
-                rules={{
-                    required: 'Campo obrigatorio',
-                    minLength: {
-                        value: 3,
-                        message: 'O nome precisa ter no mínimo 3 caracteres'
-                    }
-                }}
-                render={({ field, fieldState }) => (
-                    <TextInput
-                        errorMessage={fieldState.error?.message}
-                        value={field.value}
-                        onChangeText={field.onChange}
-                        label="Nome completo"
-                        placeholder="Nome completo"
-                        boxProps={{ mb: "s20" }}
-                    />
-                )}
+                autoCapitalize="words"
+                name="fullname"
+                rules={{ required: 'Campo obrigatório' }}
+                label="Seu nome completo"
+                placeholder="Nome completo"
+                boxProps={{ mb: "s20" }}
             />
 
-            <Controller
+            <FormTextInput
                 control={control}
+                autoCapitalize="none"
                 name="email"
-                rules={{
-                    required: 'Campo obrigatório',
-
-                }}
-                render={({ field, fieldState }) => (
-                    <TextInput
-                        errorMessage={fieldState.error?.message}
-                        value={field.value}
-                        onChangeText={field.onChange}
-                        label="E-mail"
-                        placeholder="Digite seu e-mail"
-                        boxProps={{ mb: "s20" }}
-                    />
-                )}
+                rules={{ required: 'Campo obrigatório' }}
+                label="E-mail"
+                placeholder="Digite seu e-mail"
+                boxProps={{ mb: "s20" }}
             />
 
             <Controller
