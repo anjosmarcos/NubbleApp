8a3f595399b264b5dd29cd2e186649f2068309b7
Marcos Alexandre <dev.marcosanjos@gmail.com>
2024-05-24 13:35:42 -0300

    Keep calm and commit

--- src/screens/auth/LoginScreen/loginSchema.ts
+++ src/screens/auth/LoginScreen/loginSchema.ts
@@ -1,7 +1,5 @@
 import {z} from 'zod';
 
-const teste = 12212;
-
 export const loginSchema = z.object({
   email: z.string().email('email inválido'),
   password: z.string().min(8, 'Senha deve ter no mínimo 8 caracteres'),
